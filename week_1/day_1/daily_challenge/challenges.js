90aa3d99f4f359182be585cac66c8021ab17361f
Adnane MEKTANI <adnanemektani02@gmail.com>
2026-01-28 12:10:57 +0100

    that s my challenge Not Bad and Starts

--- week_1/day_1/daily_challenge/challenges.js
+++ week_1/day_1/daily_challenge/challenges.js
@@ -1,5 +1,36 @@
-// ====== Daily Challenge 1
+// ====== Daily Challenge Not Bad
 
-// ====== Daily Challenge 2
+let sentence = "My code Was not that bad, I like it now.";
+// Create a variable called wordNot where it’s value is the first appearance (ie. the position) of the substring “not” (from the sentence variable).
+let wordNot = sentence.indexOf("not");
+let wordBad = sentence.indexOf("bad");
+if (wordBad > wordNot && wordNot !== -1) {
+    let newSentence = sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + 3);
+    console.log(newSentence);
+} else {
+    console.log(sentence);
+}
 
-// ====== Daily Challenge 3
+
+
+
+
+
+
+// ====== Daily Challenge: Stars
+
+for (let i = 1; i <=6; i++) {
+    console.log("*".repeat(i));
+}
+
+//nested loop version
+
+for (let i = 1; i <= 6; i++) {
+    let stars = ""; 
+    for (let j = 1; j <= i; j++) {
+        stars += "*"; 
+    }
+    console.log(stars);
+}
+
+
