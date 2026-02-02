9a5246445feb7837e3f480f48f6857555919a621
Adnane MEKTANI <adnanemektani02@gmail.com>
2026-02-02 14:42:59 +0100

    challenge planet

--- week_1/day_2/daily_challenge/challenges.js
+++ week_1/day_2/daily_challenge/challenges.js
@@ -1,4 +1,33 @@
 // ====== Daily Challenge 1
+let planets = [
+    { name: "Mercury", color: "grey", moons: 0 },
+    { name: "Venus", color: "orange", moons: 0 },
+    { name: "Earth", color: "blue", moons: 1 },
+    { name: "Mars", color: "red", moons: 2 }
+];
+let listPlanets = document.querySelector(".listPlanets");
+
+planets.forEach(planet => {
+    let planetDiv = document.createElement("div");
+    planetDiv.classList.add("planet");
+    planetDiv.style.backgroundColor = planet.color;
+    planetDiv.innerText = planet.name;
+    listPlanets.appendChild(planetDiv);
+
+
+
+    for (let i = 0; i < planet.moons; i++) {
+        let moonDiv = document.createElement("div");
+        moonDiv.classList.add("moon");
+        
+        moonDiv.style.left = (i * 40) + "px"; 
+
+        planetDiv.appendChild(moonDiv);
+    }
+
+
+})
+
 
 // ====== Daily Challenge 2
 
