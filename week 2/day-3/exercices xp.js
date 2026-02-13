a877ec17aa7c26fa293c5f8f623b2489ec45f9c0
Adnane MEKTANI <adnanemektani02@gmail.com>
2026-02-13 10:39:09 +0100

    exercice 1&2&3

--- week 2/day-3/exercices xp.js
+++ week 2/day-3/exercices xp.js
@@ -5,7 +5,7 @@
 
 
 //exercice 2
-
+/*
 function displayStudentInfo(objUser){
     const {first , last  }= objUser ;
     return 'ur full name is ' + first + " " + last + "." ;
@@ -13,4 +13,15 @@ function displayStudentInfo(objUser){
 }
 
 displayStudentInfo({first: 'Elie', last:'Schoppik'});
+*/
+
+
+
+//exercice 3 
+const users = { user1: 18273, user2: 92833, user3: 90315 };
+const arr = Object.entries(users);
+console.log(arr);
+
+//exercice4 
+
 
