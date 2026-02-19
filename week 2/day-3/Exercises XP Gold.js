ecd6793cd045113047bf9b943d7ca9bda4143332
Adnane MEKTANI <adnanemektani02@gmail.com>
2026-02-19 11:26:55 +0000

    Day3

--- week 2/day-3/Exercises XP Gold.js
+++ week 2/day-3/Exercises XP Gold.js
@@ -1,14 +1,14 @@
 // exercice 1
 
-/*function printFullName({first, last}){
+function printFullName({first, last}){
     return `Your full name is ${first} ${last}`;
 }
 console.log(printFullName({first: 'Elie', last:'Schoppik'}));
-*/
+
 
 
 //exercice 2
-/*
+
 function KeysAndValues(obj){
     let keys = Object.keys(obj);
     keys.sort();
@@ -23,4 +23,8 @@ function KeysAndValues(obj){
 console.log(KeysAndValues({ a: 1, b: 2, c: 3 }));
 console.log(KeysAndValues({ a: "Apple", b: "Microsoft", c: "Google" }));
 console.log(KeysAndValues({ key1: true, key2: false, key3: undefined }));
-*/
+
+
+
+//exercice3 
+// Output ==> 3
