23d5fee1e67fe5a58d2f1351e830f179c789b545
Adnane MEKTANI <adnanemektani02@gmail.com>
2026-02-20 14:30:11 +0000

    Day 4

--- week 2/day-4/exercices XP .js
+++ week 2/day-4/exercices XP .js
@@ -1,1 +1,68 @@
-//exercice 1
+//exercice 3
+
+const marioGame = {
+  detail : "An amazing game!",
+  characters : {
+      mario : {
+        description:"Small and jumpy. Likes princesses.",
+        height: 10,
+        weight: 3,
+        speed: 12,
+      },
+      bowser : {
+        description: "Big and green, Hates princesses.",
+        height: 16,
+        weight: 6,
+        speed: 4,
+      },
+      princessPeach : {
+        description: "Beautiful princess.",
+        height: 12,
+        weight: 2,
+        speed: 2,
+      }
+  },
+}
+//1
+const MariGame = JSON.stringify(marioGame)
+//Nested objects restent nested
+//all become  string
+//Keys w values prennent double quotes "
+
+console.log(MariGame)
+//Output==> {"detail":"An amazing game!","characters":{"mario":{"description":"Small and jumpy. Likes princesses.","height":10,"weight":3,"speed":12},"bowser":{"description":"Big and green, Hates princesses.","height":16,"weight":6,"speed":4},"princessPeach":{"description":"Beautiful princess.","height":12,"weight":2,"speed":2}}}
+
+
+//2
+const marioJsonPretty = JSON.stringify(marioGame, null, 2); 
+//3
+debugger;
+
+console.log(marioJsonPretty);
+//Output ==>
+ /* {
+  "detail": "An amazing game!",
+  "characters": {
+    "mario": {
+      "description": "Small and jumpy. Likes princesses.",
+      "height": 10,
+      "weight": 3,
+      "speed": 12
+    },
+    "bowser": {
+      "description": "Big and green, Hates princesses.",
+      "height": 16,
+      "weight": 6,
+      "speed": 4
+    },
+    "princessPeach": {
+      "description": "Beautiful princess.",
+      "height": 12,
+      "weight": 2,
+      "speed": 2
+    }
+  }
+}*/
+
+
+//3
