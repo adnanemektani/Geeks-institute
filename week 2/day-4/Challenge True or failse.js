59d2f2d5514b80854fd16a757c0f09caa832a232
Adnane MEKTANI <adnanemektani02@gmail.com>
2026-02-22 11:53:03 +0000

    Day4

---
+++ week 2/day-4/Challenge True or failse.js
@@ -0,0 +1,9 @@
+//exercice1
+
+function allTruthy(...args) {
+    return args.every(param => !!param);
+}
+
+console.log(allTruthy(true, true, true));  
+console.log(allTruthy(true, false, true));  
+console.log(allTruthy(5, 4, 3, 2, 1, 0));  
