d213938d2d169e6f59499beae35761e958ce7e4f
Adnane MEKTANI <adnanemektani02@gmail.com>
2026-03-09 11:49:32 +0000

    Day 4 Week3

---
+++ week_3/Day4/ExerciceXP/Exercice5/math-app/app.js
@@ -0,0 +1,6 @@
+const exercice = require("lodash")
+const { add, multiplication } = require("./math.js")
+
+add(5,3)
+multiplication(4,2)
+console.log(exercice.sum([1, 2, 3, 4]))
---
+++ week_3/Day4/ExerciceXP/Exercice5/math-app/math.js
@@ -0,0 +1,9 @@
+function add(a,b){
+    const sum = a + b ;
+    console.log(sum)
+}
+function multiplication(a, b){
+    const result = a * b;
+    console.log(result)
+}
+module.exports={add , multiplication};
