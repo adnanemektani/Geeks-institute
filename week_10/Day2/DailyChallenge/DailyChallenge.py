4202ecf1a31f86b9fde52ededcc0bfbf5b72992c
Adnane MEKTANI <adnanemektani02@gmail.com>
2026-03-28 16:58:41 +0100

    Day2OFpython

---
+++ week_10/Day2/DailyChallenge/DailyChallenge.py
@@ -0,0 +1,24 @@
+#Challenge1
+number = input("Enter a number: ")
+length = input("Enter the desired length: ")
+multiples = []
+
+for i in range(1, int(length) + 1):
+        multiples.append(int(number) * i)
+print(multiples)
+
+
+#Challenge2
+string = input("Enter a string: ")
+result = ""
+
+for i in range(len(string)):
+        letter = string[i]
+        
+        if i == 0:
+                result = result + string[i]
+
+        else:
+                if string [i] != string[i-1]:
+                        result = result + string[i]
+print(result)
