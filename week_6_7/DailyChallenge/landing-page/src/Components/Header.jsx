b47ae16abec8eb5c2e6c38d3ebe9f76a29415181
Adnane MEKTANI <adnanemektani02@gmail.com>
2026-03-28 16:29:35 +0100

    Day1 week7

---
+++ week_6_7/DailyChallenge/landing-page/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <nav>
+        <h1>Company</h1>
+        <a href="http://">Home</a> 
+        <a href="http://">Features</a>
+        <a href="http://">Contact</a>    
+    </nav>
+  )
+}
+
+export default Header  
